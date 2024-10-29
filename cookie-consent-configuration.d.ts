52bf1179ecdec745b06da7daf67e3b629a232d16
Matt Cairns <opusmatt@gmail.com>
2024-10-29 16:03:13 -0400

    add official cookie consent config

--- cookie-consent-configuration.d.ts
+++ cookie-consent-configuration.d.ts
@@ -1,40 +1,4 @@
+import { CookieConsentConfig } from "vanilla-cookieconsent";
 export declare const cookieConsentConfig: CookieConsentConfig;
 
-export declare interface CookieConsentConfig {
-  categories: {
-    [key: string]: { enabled: boolean; readOnly?: boolean };
-  };
-  guiOptions: {
-    consentModal: {
-      position: string;
-    };
-  };
-  onConsent: ({ cookie }) => void;
-  onChange: ({ cookie }) => void;
-  language: {
-    default: string;
-    translations: {
-      [key: string]: {
-        consentModal: {
-          title: string;
-          description: string;
-          acceptAllBtn: string;
-          acceptNecessaryBtn: string;
-          showPreferencesBtn: string;
-        };
-        preferencesModal: {
-          title: string;
-          acceptAllBtn: string;
-          acceptNecessaryBtn: string;
-          savePreferencesBtn: string;
-          closeIconLabel: string;
-          sections: {
-            title: string;
-            description: string;
-            linkedCategory?: string;
-          }[];
-        };
-      };
-    };
-  };
-}
+export declare type CookieConsentConfig = CookieConsentConfig;
