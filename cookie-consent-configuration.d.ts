6344f4b7924c7afe65a7dfd1597fb08252546c46
Matt Cairns <opusmatt@gmail.com>
2024-10-29 15:45:52 -0400

    feat: add types

---
+++ cookie-consent-configuration.d.ts
@@ -0,0 +1,38 @@
+export interface CookieConsentConfig {
+  categories: {
+    [key: string]: { enabled: boolean; readOnly?: boolean };
+  };
+  guiOptions: {
+    consentModal: {
+      position: string;
+    };
+  };
+  onConsent: ({ cookie }) => void;
+  onChange: ({ cookie }) => void;
+  language: {
+    default: string;
+    translations: {
+      [key: string]: {
+        consentModal: {
+          title: string;
+          description: string;
+          acceptAllBtn: string;
+          acceptNecessaryBtn: string;
+          showPreferencesBtn: string;
+        };
+        preferencesModal: {
+          title: string;
+          acceptAllBtn: string;
+          acceptNecessaryBtn: string;
+          savePreferencesBtn: string;
+          closeIconLabel: string;
+          sections: {
+            title: string;
+            description: string;
+            linkedCategory?: string;
+          }[];
+        };
+      };
+    };
+  };
+}
