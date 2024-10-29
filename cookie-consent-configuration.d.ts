1901c4f1b81b2d666d8e89c22a59643a3aa2d6dc
Matt Cairns <opusmatt@gmail.com>
2024-10-29 16:04:31 -0400

    remove pass through interface

--- cookie-consent-configuration.d.ts
+++ cookie-consent-configuration.d.ts
@@ -1,4 +1,2 @@
 import { CookieConsentConfig } from "vanilla-cookieconsent";
 export declare const cookieConsentConfig: CookieConsentConfig;
-
-export declare type CookieConsentConfig = CookieConsentConfig;
